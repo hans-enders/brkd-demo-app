642c7c2ff23fa795b3492a373c9f6d31412d78a7
viktigpetterr <viktor.grasljunga@gmail.com>
2022-11-28 21:17:31 +0100

    Add risky dependencies

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"github.com/andygrunwald/go-jira"
+	"github.com/docker/cli/cli/version"
+	"github.com/nats-io/jwt/v2"
+	"net/http"
 )
 
 func main() {
-	r := gin.Default()
-	r.Static("/", "./web/public")
-	r.Run() // listen and serve on 0.0.0.0:8080
+	fmt.Println(version.Version)
+	_, _ = jwt.DecorateJWT("token")
+	JWTAuthTransport := jira.JWTAuthTransport{nil, "debricked", &jira.BasicAuthTransport{}}
+	_, _ = JWTAuthTransport.RoundTrip(nil)
+	fs := http.FileServer(http.Dir("./web/public"))
+	http.Handle("/", fs)
 }
